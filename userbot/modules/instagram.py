02fa11750d1df84f45a9abcd9ab8df69ce31005e
azrim <mirzaspc@gmail.com>
2020-04-07 13:08:11 +0700

    FIX1

--- userbot/modules/instagram.py
+++ userbot/modules/instagram.py
@@ -79,14 +79,14 @@ async def instagram_dl(igdl):
     """ To downloading photos from instagram account """
     try:
         uname = igdl.pattern_match.group(1)
-        input_str = "tmp"
+        input_str = TEMP_DOWNLOAD_DIRECTORY
 
         if not os.path.exists(input_str):
             os.makedirs(input_str)
         else:
             await igdl.edit(f"`Getting info.....`")
             looter = ProfileLooter(f"{uname}")
-            looter.download("tmp", media_count=10)
+            looter.download(TEMP_DOWNLOAD_DIRECTORY, media_count=5)
             await igdl.edit("Processing ...")
             lst_of_files = []
             for r, d, f in os.walk(input_str):
