f8730400e8d3289a87939f06b36612b626c204aa
yincen17 <52388255+yincen17@users.noreply.github.com>
2020-04-08 23:35:44 -0700

    coba coba

--- userbot/modules/instagram.py
+++ userbot/modules/instagram.py
@@ -87,10 +87,10 @@ async def instagram_dl(igdl):
             await igdl.edit(f"`Getting info.....`")
             looter = ProfileLooter(f"{uname}")
             looter.download(TEMP_DOWNLOAD_DIRECTORY, media_count=5)
-        if ValueError:
+        except ValueError:
               await igdl.edit(f"**Account {uname} Not Found.**\nPlease enter correct username.")
               return
-        if RuntimeError:
+        except RuntimeError:
             await igdl.edit(f"**Can't Catch Media.**\nAccount {uname} is Private.")
             return
             
