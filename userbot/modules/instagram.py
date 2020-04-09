353a8427f79d645c04b57a8f5f7df58d554f6d68
yincen17 <52388255+yincen17@users.noreply.github.com>
2020-04-08 23:40:45 -0700

    coba coba

--- userbot/modules/instagram.py
+++ userbot/modules/instagram.py
@@ -93,7 +93,7 @@ async def instagram_dl(igdl):
         except RuntimeError:
             await igdl.edit(f"**Can't Catch Media.**\nAccount {uname} is Private.")
             return
-             await igdl.edit("Processing ...")
+            await igdl.edit("Processing ...")
              lst_of_files = []
              for r, d, f in os.walk(input_str):
                 for file in f:
